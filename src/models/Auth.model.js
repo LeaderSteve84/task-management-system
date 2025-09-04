e518241a9fb891d1f93536434389b9ff421bf95c
stephen_adah <mailstephenreal@gmail.com>
2025-09-04 01:18:07 +0100

    MOD: modifiefied for schema validation

--- src/models/Auth.model.js
+++ src/models/Auth.model.js
@@ -1,17 +1,22 @@
 const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-    lowercase: true,
+const userSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+      unique: true,
+      lowercase: true,
+      trim: true,
+      match: [/^\S+@\S+\.\S+$/, "Please provide a valid email address"],
+    },
+    password: {
+      type: String,
+      required: [true, "Password mis required"],
+      minlength: [8, "Password must be at least 8 characters"],
+    },
   },
-  password: {
-    type: String,
-  }
-}, {
-  timestamps: true,
-});
+  { timestamps: true }
+);
 
 module.exports = mongoose.model("User", userSchema);
