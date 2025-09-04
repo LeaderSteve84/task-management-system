f36f27ec02caf04a04921cfa380394bd1891800c
stephen_adah <mailstephenreal@gmail.com>
2025-09-04 01:52:45 +0100

    MOD: Added validateLogin

--- src/middleware/validators/userValidator.js
+++ src/middleware/validators/userValidator.js
@@ -5,7 +5,18 @@ exports.validateUser = [
   body("password")
     .isLength({ min: 8 })
     .withMessage("Password must be at least 8 characters long")
-    .matches(/[A-Z]/).withMessage("Password must contain at least one uppercase letter")
-    .matches(/[a-z]/).withMessage("Password must contain at least one lowercase letter")
-    .matches(/[0-9]/).withMessage("Password must contain at least one number"),
+    .matches(/[A-Z]/)
+    .withMessage("Password must contain at least one uppercase letter")
+    .matches(/[a-z]/)
+    .withMessage("Password must contain at least one lowercase letter")
+    .matches(/[0-9]/)
+    .withMessage("Password must contain at least one number"),
+  body("confirmPassword")
+    .custom((value, { req }) => value === req.body.password)
+    .withMessage("Confirm password must match password"),
+];
+
+exports.validateLogin = [
+  body("email").isEmail().withMessage("Valid email is required"),
+  body("password").notEmpty().withMessage("Password is required"),
 ];
